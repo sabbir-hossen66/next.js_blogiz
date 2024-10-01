4394be4b20d7610ce02539dc49e00f514800f747
sabbir <sabbir.ms2000@gmail.com>
2024-10-01 09:01:19 +0600

    added slice implement

--- src/components/LatestBlogs/LatestBlogs.tsx
+++ src/components/LatestBlogs/LatestBlogs.tsx
@@ -1,13 +1,22 @@
+import BlogCard from "../ui/BlogCard";
 import LatestBlogCard from "../ui/LatestBlogCard";
 
 
 const LatestBlogs = ({ blogs }: { blogs: Blog[] }) => {
   return (
-    <div>
+    <div className="w-[90%] mx-auto">
       <h2 className="text-center text-5xl uppercase">hello latest blogs</h2>
-      <div>
+      <div className="grid grid-cols-1 gap-4  lg:grid-cols-2 lg:gap-3">
         {
-          blogs.map(blog => <LatestBlogCard
+          blogs.slice(0, 2).map(blog => <LatestBlogCard
+            key={blog.id}
+            blog={blog}
+          />)
+        }
+      </div>
+      <div className="grid grid-cols-1 gap-4  lg:grid-cols-3 lg:gap-3 mx-auto">
+        {
+          blogs.slice(2, 5).map(blog => <BlogCard
             key={blog.id}
             blog={blog}
           />)
